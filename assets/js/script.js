1b0571b368cf7d757e78b1a12489b957f5f69a6c
My Name <dlm1337.dm@gmail.com>
2024-05-13 13:35:00 -0400

    update ui/ux new colors and stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
 "use strict";
 
-// element toggle function
 const elementToggleFunc = function (elem) {
   elem.classList.toggle("active");
 };
 
-// sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 const mainContent = document.querySelector("[main-content]");
 
-// sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () {
   elementToggleFunc(sidebar);
 });
 
-// Get the gallery box
 var img1 = document.getElementById("img1");
 var img2 = document.getElementById("img2");
 var img3 = document.getElementById("img3");
@@ -23,12 +19,10 @@ var img4 = document.getElementById("img4");
 var img5 = document.getElementById("img5");
 var img6 = document.getElementById("img6");
 
-// Get the modal image tag
 var modal = document.getElementById("myModal");
 
 var modalImage = document.getElementById("portfolio-modal-image");
 
-// When the user clicks the big picture, set the image and open the modal
 img1.onclick = function () {
   var src = img1.lastElementChild.src;
   modal.style.display = "block";
@@ -60,13 +54,10 @@ img6.onclick = function (e) {
   modalImage.src = src;
 };
 
-// testimonials variables
-//const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
@@ -77,38 +68,10 @@ portfolioModalClose.addEventListener("click", async () => {
   modal.style.display = "none";
 });
 
-// modal toggle function
-// const testimonialsModalFunc = function () {
-//   modalContainer.classList.toggle("active");
-//   overlay.classList.toggle("active");
-// };
-
-// // add click event to all modal items
-// for (let i = 0; i < testimonialsItem.length; i++) {
-//   testimonialsItem[i].addEventListener("click", function () {
-//     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-//     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-//     modalTitle.innerHTML = this.querySelector(
-//       "[data-testimonials-title]"
-//     ).innerHTML;
-//     modalText.innerHTML = this.querySelector(
-//       "[data-testimonials-text]"
-//     ).innerHTML;
-
-//     testimonialsModalFunc();
-//   });
-// }
-
-// add click event to modal close button
-//modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-//overlay.addEventListener("click", testimonialsModalFunc);
-
-// custom select variables
 const selectItems = document.querySelectorAll("[data-select-item]");
 const selectValue = document.querySelector("[data-selecct-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
-// filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
@@ -123,7 +86,6 @@ const filterFunc = function (selectedValue) {
   }
 };
 
-// add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
@@ -138,11 +100,9 @@ for (let i = 0; i < filterBtn.length; i++) {
   });
 }
 
-// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
     for (let i = 0; i < pages.length; i++) {
